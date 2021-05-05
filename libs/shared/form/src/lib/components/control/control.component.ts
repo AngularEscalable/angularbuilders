6f40683f7aba00538257903cb9c177e9283ca102
Alberto Basalo <albertobasalo@hotmail.com>
2021-05-05 12:43:21 +0200

    42 cva

--- libs/shared/form/src/lib/components/control/control.component.ts
+++ libs/shared/form/src/lib/components/control/control.component.ts
@@ -1,19 +1,55 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  forwardRef,
+  Input,
+} from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormGroup,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
 
 @Component({
   selector: 'ab-form-control',
   templateUrl: './control.component.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ControlComponent),
+      multi: true,
+    },
+  ],
 })
-export class ControlComponent {
+export class ControlComponent implements ControlValueAccessor {
   @Input() form!: FormGroup;
   @Input() formControlName = '';
   @Input() label = '';
   @Input() placeholder = '';
   @Input() type = 'text';
+
+  value: unknown;
+  emitChange: any;
+  emitTouch: any;
   constructor() {}
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+  registerOnChange(fn: any): void {
+    this.emitChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.emitTouch = fn;
+  }
+
+  onInput(event: any) {
+    this.value = event.target.value;
+    this.emitChange(this.value);
+    this.emitTouch();
+  }
 
   hasErrorToShow() {
     const control = this.form.controls[this.formControlName];
